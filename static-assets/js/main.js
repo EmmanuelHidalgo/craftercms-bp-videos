5b2eaf00f6616f53986d67eef04592a31d7fc0cf
Emmanuel Hidalgo <emmanuelhidalgo@Emmanuel-Hidalgos-MacBook-Pro.local>
2018-01-11 17:55:44 -0600

    Add responsive desingn to the carousel

--- static-assets/js/main.js
+++ static-assets/js/main.js
@@ -39,9 +39,36 @@ function generateChildren(parentChildren) {
           dots: true,
           infinite: true,
           arrows: true,
-          centerMode: true,
-          mobileFirst: true,
-          autoplay: true
+ 
+          autoplay: true,
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
       });
       $("#form-submit").click(function(e){
               e.preventDefault();
